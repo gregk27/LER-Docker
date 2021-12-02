a82eb3576d8b9e1e98d044550458df676eca66a5
Gregk27 <git@gregk.ca>
2021-12-02 18:00:04 -0500

    Update buildscript to make images for all years specified in versions.csv

--- build.sh
+++ build.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-# Script to build and publish the docker image for a given year, syntax is: build YEAR WPI_VERSION
+# Script to build and publish the docker image for all years listed in versions.csv
 
-if [ -z "$1" ] || [ -z "$2" ]
-  then
-    echo "Missing argument(s), should be in form: build YEAR WPI_VERSION"
-    exit 1
-fi
+# Build a version, args are year, wpi version, and tag 
+function build {
+    echo Building $3
+    docker build --build-arg YEAR=$1 --build-arg WPI_VERSION=$2 -t $3 .
+    docker push $3
+}
 
-docker build --build-arg YEAR=$1 --build-arg WPI_VERSION=$2 -t gregk27/ler-programming:$1 .
-docker push gregk27/ler-programming:$1
+years=()
+wpiVersions=()
+#Read version configuration
+while IFS=',' read -r col1 col2
+do
+    years+=($col1)
+    wpiVersions+=($col2)
+done < ./versions.csv
+
+i=0
+for year in ${years[@]}; do
+    tag="gregk27/ler-programming:$year"
+    build $year ${wpiVersions[i]} $tag
+
+    for ((j=$((wpiVersions[i]));j>0;j--)) do
+        tag="gregk27/ler-programming:$year-$j"
+        build $year $j $tag
+    done
+    i=$i+1
+done
